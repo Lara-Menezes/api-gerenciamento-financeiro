1f7bba91806b994828b5c5b4a5cd01fa21f936c9
Lara Bezerra de Menezes <larabdmenezes@gmail.com>
2025-07-12 22:23:08 -0300

    chore: add documentação da API com Swagger

--- fintech-pagamento/src/main/java/org/example/config/SwaggerConfig.java
+++ fintech-pagamento/src/main/java/org/example/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package org.example.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Gestão de Clientes e Faturas")
+                        .version("1.0.0")
+                        .description("Documentação da API para gerenciamento de clientes, faturas e pagamentos."));
+    }
 }
